947d47b262d698ceca7fe819917734e64044e235
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-03-01 15:32:26 +0100

    po hodině

--- Attendance_IT3A/Form1.Designer.cs
+++ Attendance_IT3A/Form1.Designer.cs
@@ -28,13 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lboxAll = new System.Windows.Forms.ListBox();
+            this.lboxPerson = new System.Windows.Forms.ListBox();
+            this.lboxRecord = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnShiftStart = new System.Windows.Forms.Button();
+            this.btnShiftStop = new System.Windows.Forms.Button();
+            this.btnPauseStart = new System.Windows.Forms.Button();
+            this.btnPauseEnd = new System.Windows.Forms.Button();
+            this.btnDoctorStart = new System.Windows.Forms.Button();
+            this.btnDoctorEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lboxAll
+            // 
+            this.lboxAll.FormattingEnabled = true;
+            this.lboxAll.ItemHeight = 25;
+            this.lboxAll.Location = new System.Drawing.Point(12, 56);
+            this.lboxAll.Name = "lboxAll";
+            this.lboxAll.Size = new System.Drawing.Size(217, 454);
+            this.lboxAll.TabIndex = 0;
+            // 
+            // lboxPerson
+            // 
+            this.lboxPerson.FormattingEnabled = true;
+            this.lboxPerson.ItemHeight = 25;
+            this.lboxPerson.Location = new System.Drawing.Point(235, 56);
+            this.lboxPerson.Name = "lboxPerson";
+            this.lboxPerson.Size = new System.Drawing.Size(217, 454);
+            this.lboxPerson.TabIndex = 0;
+            this.lboxPerson.SelectedIndexChanged += new System.EventHandler(this.lboxPerson_SelectedIndexChanged);
+            // 
+            // lboxRecord
+            // 
+            this.lboxRecord.FormattingEnabled = true;
+            this.lboxRecord.ItemHeight = 25;
+            this.lboxRecord.Location = new System.Drawing.Point(458, 56);
+            this.lboxRecord.Name = "lboxRecord";
+            this.lboxRecord.Size = new System.Drawing.Size(217, 454);
+            this.lboxRecord.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Všechno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Osoby";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(458, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Záznamy";
+            // 
+            // btnShiftStart
+            // 
+            this.btnShiftStart.BackColor = System.Drawing.Color.Lime;
+            this.btnShiftStart.Location = new System.Drawing.Point(681, 56);
+            this.btnShiftStart.Name = "btnShiftStart";
+            this.btnShiftStart.Size = new System.Drawing.Size(193, 69);
+            this.btnShiftStart.TabIndex = 2;
+            this.btnShiftStart.Text = "Zahájení směny";
+            this.btnShiftStart.UseVisualStyleBackColor = false;
+            this.btnShiftStart.Click += new System.EventHandler(this.btnShiftStart_Click);
+            // 
+            // btnShiftStop
+            // 
+            this.btnShiftStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnShiftStop.Location = new System.Drawing.Point(681, 131);
+            this.btnShiftStop.Name = "btnShiftStop";
+            this.btnShiftStop.Size = new System.Drawing.Size(193, 69);
+            this.btnShiftStop.TabIndex = 2;
+            this.btnShiftStop.Text = "Ukončení směny";
+            this.btnShiftStop.UseVisualStyleBackColor = false;
+            this.btnShiftStop.Click += new System.EventHandler(this.btnShiftStop_Click);
+            // 
+            // btnPauseStart
+            // 
+            this.btnPauseStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnPauseStart.Location = new System.Drawing.Point(681, 206);
+            this.btnPauseStart.Name = "btnPauseStart";
+            this.btnPauseStart.Size = new System.Drawing.Size(193, 69);
+            this.btnPauseStart.TabIndex = 2;
+            this.btnPauseStart.Text = "Zahájení pauzy";
+            this.btnPauseStart.UseVisualStyleBackColor = false;
+            this.btnPauseStart.Click += new System.EventHandler(this.btnPauseStart_Click);
+            // 
+            // btnPauseEnd
+            // 
+            this.btnPauseEnd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnPauseEnd.Location = new System.Drawing.Point(681, 281);
+            this.btnPauseEnd.Name = "btnPauseEnd";
+            this.btnPauseEnd.Size = new System.Drawing.Size(193, 69);
+            this.btnPauseEnd.TabIndex = 2;
+            this.btnPauseEnd.Text = "Ukončení pauzy";
+            this.btnPauseEnd.UseVisualStyleBackColor = false;
+            this.btnPauseEnd.Click += new System.EventHandler(this.btnPauseEnd_Click);
+            // 
+            // btnDoctorStart
+            // 
+            this.btnDoctorStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnDoctorStart.Location = new System.Drawing.Point(681, 356);
+            this.btnDoctorStart.Name = "btnDoctorStart";
+            this.btnDoctorStart.Size = new System.Drawing.Size(193, 69);
+            this.btnDoctorStart.TabIndex = 2;
+            this.btnDoctorStart.Text = "Odchod k lékaři";
+            this.btnDoctorStart.UseVisualStyleBackColor = false;
+            this.btnDoctorStart.Click += new System.EventHandler(this.btnDoctorStart_Click);
+            // 
+            // btnDoctorEnd
+            // 
+            this.btnDoctorEnd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnDoctorEnd.Location = new System.Drawing.Point(681, 431);
+            this.btnDoctorEnd.Name = "btnDoctorEnd";
+            this.btnDoctorEnd.Size = new System.Drawing.Size(193, 69);
+            this.btnDoctorEnd.TabIndex = 2;
+            this.btnDoctorEnd.Text = "Návrat od lékaře";
+            this.btnDoctorEnd.UseVisualStyleBackColor = false;
+            this.btnDoctorEnd.Click += new System.EventHandler(this.btnDoctorEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(886, 534);
+            this.Controls.Add(this.btnDoctorEnd);
+            this.Controls.Add(this.btnDoctorStart);
+            this.Controls.Add(this.btnPauseEnd);
+            this.Controls.Add(this.btnPauseStart);
+            this.Controls.Add(this.btnShiftStop);
+            this.Controls.Add(this.btnShiftStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lboxRecord);
+            this.Controls.Add(this.lboxPerson);
+            this.Controls.Add(this.lboxAll);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lboxAll;
+        private System.Windows.Forms.ListBox lboxPerson;
+        private System.Windows.Forms.ListBox lboxRecord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnShiftStart;
+        private System.Windows.Forms.Button btnShiftStop;
+        private System.Windows.Forms.Button btnPauseStart;
+        private System.Windows.Forms.Button btnPauseEnd;
+        private System.Windows.Forms.Button btnDoctorStart;
+        private System.Windows.Forms.Button btnDoctorEnd;
     }
 }
 
--- Attendance_IT3A/Form1.cs
+++ Attendance_IT3A/Form1.cs
@@ -12,9 +12,106 @@ namespace Attendance_IT3A
 {
     public partial class Form1 : Form
     {
+        List<Person> people;
+        List<Record> records;
+        SqlRepository SqlRepository = new SqlRepository();
+
         public Form1()
         {
             InitializeComponent();
+            SetButtons(false);
+            people = SqlRepository.GetPeople();
+            records = new List<Record>();
+            foreach (Person person in people)
+            {
+                records.AddRange(person.Records);
+            }
+            RefreshGUI();
+        }
+
+        private void RefreshGUI()
+        {
+            lboxAll.Items.Clear();
+            lboxPerson.Items.Clear();
+            lboxRecord.Items.Clear();
+            foreach (Person person in people)
+            {
+                lboxPerson.Items.Add($"{person.PersonalNumber} - {person.Lastname} {person.Firstname}");
+            }
+            foreach (Record record in records.OrderByDescending(p => p.DateTime))
+            {
+                lboxAll.Items.Add($"{record.DateTime.ToString("dd.MM hh:mm")} - {record.Person.Lastname} - {record.Reason}");
+            }
+        }
+
+        private void lboxPerson_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lboxPerson.SelectedIndex;
+            if (index >= 0)
+            {
+                SetButtons(true);
+                lboxRecord.Items.Clear();
+                foreach (Record record in people[index].Records.OrderByDescending(p => p.DateTime))
+                {
+                    lboxRecord.Items.Add($"{record.DateTime} - {record.Reason}");
+                }
+            }
+            else
+            {
+                SetButtons(false);
+            }
+        }
+
+        private void SetButtons(bool enabled)
+        {
+            btnDoctorEnd.Enabled = enabled;
+            btnDoctorStart.Enabled = enabled;
+            btnPauseStart.Enabled = enabled;
+            btnPauseEnd.Enabled = enabled;
+            btnShiftStart.Enabled = enabled;
+            btnShiftStop.Enabled = enabled;
+        }
+
+        private void btnShiftStart_Click(object sender, EventArgs e)
+        {
+            int index = lboxPerson.SelectedIndex;
+            if (index >= 0)
+            {
+                Record newRecord = new Record()
+                {
+                    DateTime = DateTime.Now,
+                    Person = people[index],
+                    Reason = RecordReason.StartShift
+                };
+                records.Add(newRecord);
+                people[index].Records.Add(newRecord);
+                RefreshGUI();
+            }
+        }
+
+        private void btnShiftStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPauseStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPauseEnd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDoctorStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDoctorEnd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
